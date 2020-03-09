cfe41d8ab14cd4c0c238f0821ebcef141e9505e7
ico1036 <jiwoong.kim@cern.sh>
2020-03-09 22:06:50 +0900

    v1

--- crawling/.ipynb_checkpoints/crawler-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen("https://jiwoong.web.cern.ch/jiwoong/cgi-bin/igprof-navigator/igprofCPU_")  
-bsObject = BeautifulSoup(html, "html.parser") 
-
-
-print(bsObject) # 웹 문서 전체가 출력됩니다. 
