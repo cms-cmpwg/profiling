18fc94a1fb8b5611436bcc57c85623cc4af29bd0
Patrick Gartung <gartung@fnal.gov>
2020-04-16 20:28:38 +0200

    Bash vars can't use -

--- run-igprof-cron.sh
+++ run-igprof-cron.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-workdir=/home/users/gartung
-eoswwwdir=/eos/usr/g/gartung/www
-last-nightly=$(cat ${workdir}/last-nightly)
-latest-nightly=$(scram list CMSSW_1 | grep CMSSW_ | sort | tail -1 | awk '{print $2}')
-if [[ "${last-nightly}" != "${latest-nightly}" ]];then
-     cd ${workdir}
-     ${workdir}/ServiceWork/Gen_tool/Gen.sh ${latest-nightly}
-     ${workdir}/ServiceWork/Gen_tool/runall_mem.sh ${latest-nightly}
-     ${workdir}/ServiceWork/Gen_tool/runall_cpu.sh ${latest-nightly}
-     cd ${workdir}/${latest-nightly}/src/TimeMemory
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+WORKDIR=/home/users/gartung
+EOSWWWDIR=/eos/user/g/gartung/www
+LASTNIGHTLY=`cat ${WORKDIR}/last-nightly`
+echo $LASTNIGHTLY
+LATESTNIGHTLY=`scram list CMSSW_1 | grep CMSSW_ | sort | tail -1 | awk '{print $2}'`
+echo $LATESTNIGHTLY
+if [[ "${LASTNIGHTLY}" != "${LATESTNIGHTLY}" ]];then
+     cd ${WORKDIR}
+     ${WORKDIR}/ServiceWork/Gen_tool/Gen.sh ${LATESTNIGHTLY}
+     ${WORKDIR}/ServiceWork/Gen_tool/runall_mem.sh ${LATESTNIGHTLY}
+     ${WORKDIR}/ServiceWork/Gen_tool/runall_cpu.sh ${LATESTNIGHTLY}
+     cd ${WORKDIR}/${LATESTNIGHTLY}/src/TimeMemory
      ./profile.sh
-     cd ${workdir}
+     cd ${WORKDIR}
+     echo ${LATESTNIGHTLY} > ${WORKDIR}/last-nightly
+     mkdir -p ${EOSWWWDIR}/results/${LATESTNIGHTLY}
+     cp -pv ${WORKDIR}/${LATESTNIGHTLY}/src/TimeMemory/*.res ${EOSWWWDIR}/results/${LATESTNIGHTLY}
+     cp -pv ${WORKDIR}/${LATESTNIGHTLY}/src/TimeMemory/*.sql3 ${EOSWWWDIR}/cgi-bin/data
 fi
-echo ${latest-nightly} > ${workdir}/last-nightly
-mkdir -p ${eoswwwdir}/results/${latest-nightly}
-cp -pv ${workdir}/${latest-nightly}/src/TimeMemory/*.res ${eoswwwdir}/results/${latest-nightly}
-cp -pv ${workdir}/${latest-nightly}/src/TimeMemory/*.sql3 ${eoswwwdir}/cgi-bin/data
