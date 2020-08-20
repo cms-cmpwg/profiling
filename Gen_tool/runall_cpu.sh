651af310e20bcca9f7091ee9e4a7d56cc38ab2e6
Patrick Gartung <gartung@fnal.gov>
2020-08-20 20:27:25 +0200

    cd to

--- Gen_tool/runall_cpu.sh
+++ Gen_tool/runall_cpu.sh
@@ -9,9 +9,7 @@ export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 echo "$VO_CMS_SW_DIR $SCRAM_ARCH"
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
-echo "Start install $CMSSW_v ..."
-#scramv1 project $CMSSW_v
-cd $CMSSW_v/src
+cd $WORKSPACE/$CMSSW_v/src
 eval `scramv1 runtime -sh`
 cd TimeMemory
 echo "My loc"
