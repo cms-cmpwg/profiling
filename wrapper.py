462f97145d0617ebeec2dab8cefda72a4d488e6e
Patrick Gartung <gartung@fnal.gov>
2020-08-26 23:23:43 +0200

    Fix typo.

--- wrapper.py
+++ wrapper.py
@@ -6,3 +6,5 @@ outputs = process.outputModules_()
 for output in outputs:
 	setattr(process,output,cms.OutputModule("AsciiOutputModule", outputCommands = getattr(getattr(process,output),"outputCommands")))
 process.options.numberOfThreads = cms.untracked.uint32(1)
+process.FastTimerService.writeJSONSummary = cms.untracked.bool(True)
+process.FastTimerService.jsonFileName = cms.untracked.string('%s.resources.json' % config)
