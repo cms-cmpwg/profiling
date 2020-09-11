207e484e87d82a7fdfe9d80902bc05be8a967c9b
Patrick Gartung <gartung@fnal.gov>
2020-09-11 10:59:40 -0500

    Only run igprof on RECO and AOD steps for profiling.

--- Gen_tool/Gen.sh
+++ Gen_tool/Gen.sh
@@ -46,11 +46,11 @@ fi
 
 if [ "X$WORKSPACE" != "X" ];then
 #running on Jenkins WORKSPACE is defined and we want to generate and run the config files
-  runTheMatrix.py -w upgrade -l $PROFILING_WORKFLOW --ibeos --command=--number=$EVENTS\ --nThreads=$NTHREADS\ --customise=Validation/Performance/TimeMemoryInfo.py #200PU for 11_2_X
+  runTheMatrix.py -w upgrade -l $PROFILING_WORKFLOW --ibeos --command=--number=$EVENTS\ --nThreads=$NTHREADS\ --customise=Validation/Performance/TimeMemoryInfo.py\ --dirin=$WORKSPACE\ --dirout=$WORKSPACE #200PU for 11_2_X
   outname=$(ls -d ${PROFILING_WORKFLOW}*) 
   mkdir -p TimeMemory
   mv $outname TimeMemory/$PROFILING_WORKFLOW
-  runTheMatrix.py -w upgrade -l $PROFILING_WORKFLOW --ibeos --command=--number=$EVENTS\ --nThreads=$NTHREADS\ --customise=HLTrigger/Timer/FastTimer.customise_timer_service_singlejob\ --no_exec  #200PU for 11_2_X
+  runTheMatrix.py -w upgrade -l $PROFILING_WORKFLOW --ibeos --command=--number=$EVENTS\ --nThreads=$NTHREADS\ --customise=HLTrigger/Timer/FastTimer.customise_timer_service_singlejob\ --no_exec\ --dirin=$WORKSPACE\ --dirout=$WORKSPACE  #200PU for 11_2_X
   outname=$(ls -d ${PROFILING_WORKFLOW}*) 
   mv $outname $PROFILING_WORKFLOW
   cd $PROFILING_WORKFLOW
