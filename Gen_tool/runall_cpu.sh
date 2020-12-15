8273a37ed8aa21c3f16ed3905ec36b1d9be384e7
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2020-12-15 08:55:32 +0100

    set env when not under jenkins (i.e. WORKSPACE not set)

--- Gen_tool/runall_cpu.sh
+++ Gen_tool/runall_cpu.sh
@@ -12,9 +12,6 @@ else
 fi
 echo "Your SCRAM_ARCH $SCRAM_ARCH"
 
-export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
-source $VO_CMS_SW_DIR/cmsset_default.sh
-
 if [ "X$PROFILING_WORKFLOW" == "X" ];then
   export PROFILING_WORKFLOW="23434.21"
 fi 
@@ -22,9 +19,11 @@ fi
 if [ "X$WORKSPACE" != "X" ]; then
   cd $WORKSPACE/$CMSSW_v/$PROFILING_WORKFLOW
 else
+  export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+  source $VO_CMS_SW_DIR/cmsset_default.sh
   cd $CMSSW_v/TimeMemory
+  eval `scramv1 runtime -sh`
 fi
-eval `scramv1 runtime -sh`
 
 echo "My loc"
 echo $PWD
