1a8665bf876718e6981fc3a17fffbed47166a9d7
Patrick Gartung <gartung@fnal.gov>
2022-03-10 20:02:23 +0100

    Cap std:err to log as well

--- Gen_tool/Gen_vtune.sh
+++ Gen_tool/Gen_vtune.sh
@@ -69,10 +69,10 @@ fi
 source /cvmfs/projects.cern.ch/intelsw/oneAPI/linux/x86_64/2022/vtune/latest/vtune-vars.sh
 CMSRUN=`which cmsRun`
 VTUNE=`which vtune`
-$VTUNE -collect hotspots -data-limit=0 -knob enable-stack-collection=true -knob stack-size=4096 -knob sampling-mode=sw -- $CMSRUN $(ls TTbar*.py) >step1.log
-$VTUNE -collect hotspots -data-limit=0 -knob enable-stack-collection=true -knob stack-size=4096 -knob sampling-mode=sw -- $CMSRUN $(ls step2*.py) >step2.log
-$VTUNE -collect hotspots -data-limit=0 -knob enable-stack-collection=true -knob stack-size=4096 -knob sampling-mode=sw -- $CMSRUN $(ls step3*.py) >step3.log
-$VTUNE -collect hotspots -data-limit=0 -knob enable-stack-collection=true -knob stack-size=4096 -knob sampling-mode=sw -- $CMSRUN $(ls step4*.py) >step4.log
+$VTUNE -collect hotspots -data-limit=0 -knob enable-stack-collection=true -knob stack-size=4096 -knob sampling-mode=sw -- $CMSRUN $(ls TTbar*.py) >step1.log 2>&1 
+$VTUNE -collect hotspots -data-limit=0 -knob enable-stack-collection=true -knob stack-size=4096 -knob sampling-mode=sw -- $CMSRUN $(ls step2*.py) >step2.log 2>&1 
+$VTUNE -collect hotspots -data-limit=0 -knob enable-stack-collection=true -knob stack-size=4096 -knob sampling-mode=sw -- $CMSRUN $(ls step3*.py) >step3.log 2>&1 
+$VTUNE -collect hotspots -data-limit=0 -knob enable-stack-collection=true -knob stack-size=4096 -knob sampling-mode=sw -- $CMSRUN $(ls step4*.py) >step4.log 2>&1 
 $VTUNE -report gprof-cc -r r000hs -format=csv -csv-delimiter=semicolon >r000hs.gprof_cc.csv
 $VTUNE -report gprof-cc -r r001hs -format=csv -csv-delimiter=semicolon >r001hs.gprof_cc.csv
 $VTUNE -report gprof-cc -r r002hs -format=csv -csv-delimiter=semicolon >r002hs.gprof_cc.csv
