19152e658c09154be9512bd0d4f91ad120fe9c5a
Patrick Gartung <gartung@fnal.gov>
2022-11-24 04:31:12 +0100

    Remove olf files

--- circles-wrapper.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-import sys,os
-sys.path.append(os.path.dirname(sys.argv[-1]))
-config = os.path.basename(sys.argv[-1]).replace(".py","")
-process = getattr(__import__(config,fromlist=["process"]),"process")
-process.FastTimerService.writeJSONSummary = cms.untracked.bool(True)
-process.FastTimerService.jsonFileName = cms.untracked.string('%s.resources.json' % config)
