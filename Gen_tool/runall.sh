fee4ebf60579a2a9f47b8c9ef90bfd511216c03d
Patrick Gartung <gartung@fnal.gov>
2022-12-11 03:43:16 +0100

    Don't look for files in

--- Gen_tool/runall.sh
+++ Gen_tool/runall.sh
@@ -117,12 +117,6 @@ else
 fi
 
 echo generating products sizes files
-if [ "X$WORKSPACE" != "X" ];then
-  if [ -f ${WORKSPACE}/step3.root ]; then edmEventSize -v ${WORKSPACE}/step3.root > step3_sizes_${PROFILING_WORKFLOW}.txt; else echo no step3.root;fi
-  if [ -f ${WORKSPACE}/step4.root ]; then edmEventSize -v ${WORKSPACE}/step4.root > step4_sizes_${PROFILING_WORKFLOW}.txt; else echo no step4.root;fi
-  if [ -f ${WORKSPACE}/step5.root ]; then edmEventSize -v ${WORKSPACE}/step5.root > step5_sizes_${PROFILING_WORKFLOW}.txt; else echo no step5.root; fi
-else
   if [ -f step3.root ]; then edmEventSize -v step3.root > step3_sizes_${PROFILING_WORKFLOW}.txt; else echo no step3.root; fi
   if [ -f step4.root ]; then edmEventSize -v step4.root > step4_sizes_${PROFILING_WORKFLOW}.txt; else echo no step4.root; fi
   if [ -f step5.root ]; then edmEventSize -v step5.root > step5_sizes_${PROFILING_WORKFLOW}.txt; else echo no step5.root; fi
-fi
