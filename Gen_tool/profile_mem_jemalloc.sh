bd87cfc066d3fbbde66d34b3132bbe15dc00f1c8
Patrick Gartung <gartung@fnal.gov>
2023-05-03 18:26:21 -0500

    Specify the path to jeprof

--- Gen_tool/profile_mem_jemalloc.sh
+++ Gen_tool/profile_mem_jemalloc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-scram setup jemalloc-prof
-scram b ToolUpdated
 PATH=$PATH:$JEMALLOC_PROF_BASE/bin
+which jeprof
 for f in $(ls *.heap 2>/dev/null);do
-  jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
+  $JEMALLOC_PROF_BASE/bin/jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
 done
