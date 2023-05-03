625dc3ba74469d2a1be27b5ac1ee2b6825aa07a1
Patrick Gartung <gartung@fnal.gov>
2023-05-04 00:25:23 +0200

    Add jamalloc-prof package bin path to /cvmfs/cms.cern.ch/share/overrides/bin:/home/users/gartung/CMSSW_13_1_0_pre3/bin/slc7_amd64_gcc11:/home/users/gartung/CMSSW_13_1_0_pre3/external/slc7_amd64_gcc11/bin:/cvmfs/cms.cern.ch/slc7_amd64_gcc11/cms/cmssw/CMSSW_13_1_0_pre3/bin/slc7_amd64_gcc11:/cvmfs/cms.cern.ch/slc7_amd64_gcc11/cms/cmssw/CMSSW_13_1_0_pre3/external/slc7_amd64_gcc11/bin:/cvmfs/cms.cern.ch/slc7_amd64_gcc11/external/llvm/12.0.1-7b69b159e657685ca50802da2ae80a0e/bin:/cvmfs/cms.cern.ch/slc7_amd64_gcc11/external/gcc/11.2.1-f9b9dfdd886f71cd63f5538223d8f161/bin:/afs/cern.ch/cms/caf/scripts:/cvmfs/cms.cern.ch/common:/usr/sue/bin:/usr/local/cuda-11.8/bin/:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/puppetlabs/bin:/afs/cern.ch/user/g/gartung/.local/bin:/afs/cern.ch/user/g/gartung/bin

--- Gen_tool/profile_mem_jemalloc.sh
+++ Gen_tool/profile_mem_jemalloc.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+scram setup jemalloc-prof
+scram b ToolUpdated
+PATH=$PATH:$JEMALLOC_PROF_BASE/bin
 for f in $(ls *.heap 2>/dev/null);do
   jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
 done
