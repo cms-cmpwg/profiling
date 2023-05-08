ff532ebc2817dd3edcb7b75e9bd3646c3a65dfd6
Patrick Gartung <gartung@fnal.gov>
2023-05-08 17:30:22 +0200

    Try JEMALLOC_BASE

--- Gen_tool/profile_mem_jemalloc.sh
+++ Gen_tool/profile_mem_jemalloc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 scram setup jemalloc-prof
 scram b ToolUpdated
-PATH=$PATH:$JEMALLOC_PROF_BASE/bin
+eval `scram run -sh`
+PATH=$PATH:$JEMALLOC_BASE/bin
 which jeprof
 for f in $(ls *.heap 2>/dev/null);do
-  $JEMALLOC_PROF_BASE/bin/jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
+  $JEMALLOC_BASE/bin/jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
 done
