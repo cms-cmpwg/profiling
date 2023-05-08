1b95b5667a2206cebf5583fa4768add25f293bde
Patrick Gartung <gartung@fnal.gov>
2023-05-08 19:52:51 +0200

    Use scram tool info to get jemalloc bin dir

--- Gen_tool/profile_mem_jemalloc.sh
+++ Gen_tool/profile_mem_jemalloc.sh
@@ -2,8 +2,8 @@
 scram setup jemalloc-prof
 scram b ToolUpdated
 eval `scram run -sh`
-PATH=$PATH:$JEMALLOC_BASE/bin
+PATH=$PATH:$(scram tool info jemalloc | grep BINDIR | cut -d= -f2)
 which jeprof
 for f in $(ls *.heap 2>/dev/null);do
-  $JEMALLOC_BASE/bin/jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
+  jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
 done
