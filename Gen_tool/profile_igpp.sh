e091e2af30054964d526b91d15100850d74c1f08
Patrick Gartung <gartung@fnal.gov>
2023-05-30 17:17:10 +0200

    Need uncompressed txt report for event loop extraction.

--- Gen_tool/profile_igpp.sh
+++ Gen_tool/profile_igpp.sh
@@ -14,8 +14,10 @@ for f in $(ls *CPU*.gz 2>/dev/null);do
     igprof-analyse  -v -d -g $f | gzip -c > $txtf 2>> $logf 3>&2
 done
 
-if [ -f RES_CPU_step3.txt ]; then
+if [ -f RES_CPU_step3.txt.gz ]; then
+  gzip -dc RES_CPU_step3.txt.gz >RES_CPU_step3.txt
   export IGREP=RES_CPU_step3.txt
   export IGSORT=sorted_RES_CPU_step3.txt
   awk -v module=doEvent 'BEGIN { total = 0; } { if(substr($0,0,1)=="-"){good = 0;}; if(good&&length($0)>0){print $0; total += $3;}; if(substr($0,0,1)=="["&&index($0,module)!=0) {good = 1;} } END { print "Total: "total } ' ${IGREP} | sort -n -r -k1 | awk '{ if(index($0,"Total: ")!=0){total=$0;} else{print $0;} } END { print total; }' > ${IGSORT} 2>&1
+  rm RES_CPU_step3.txt
 fi
