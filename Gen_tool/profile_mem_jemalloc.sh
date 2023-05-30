83e8a58e986f45c88ae66380d219b037e9a81315
Patrick Gartung <gartung@fnal.gov>
2023-05-30 08:59:20 -0500

    Jemalloc self and cumulative reports. Nsys only gpu* reports

--- Gen_tool/profile_mem_jemalloc.sh
+++ Gen_tool/profile_mem_jemalloc.sh
@@ -5,6 +5,7 @@ eval `scram run -sh`
 PATH=$PATH:$(scram tool info jemalloc | grep BINDIR | cut -d= -f2)
 which jeprof
 for f in $(ls *.heap 2>/dev/null);do
-  jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
+  jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.cum.txt
+  jeprof --text --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
   gzip $f
 done
