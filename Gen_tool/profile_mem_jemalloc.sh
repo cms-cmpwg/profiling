c7e4f19ea04c9b200df5f80a7177b2c467bab7d9
Patrick Gartung <gartung@fnal.gov>
2023-08-30 09:57:02 -0500

    Run the jeprof commands in parallel

--- Gen_tool/profile_mem_jemalloc.sh
+++ Gen_tool/profile_mem_jemalloc.sh
@@ -5,7 +5,6 @@ eval `scram run -sh`
 PATH=$PATH:$(scram tool info jemalloc | grep BINDIR | cut -d= -f2)
 which jeprof
 for f in $(ls *.heap 2>/dev/null);do
-#  jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.cum.txt
-#  jeprof --text --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt
-  gzip $f
+  jeprof --text --cum --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.cum.txt && jeprof --text --show_bytes --exclude="(jeprof_*|prof_*|fallback*)" `which cmsRunJEProf` $f >$f.txt && gzip $f &
 done
+wait
