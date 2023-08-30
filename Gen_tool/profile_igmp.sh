e71c3f7c147b2c7cae4db8fff51b8d408c1d6f15
Patrick Gartung <gartung@fnal.gov>
2023-08-30 10:17:55 -0500

    Run igprof-analyze in parallel

--- Gen_tool/profile_igmp.sh
+++ Gen_tool/profile_igmp.sh
@@ -7,10 +7,10 @@ for f in $(ls *MEM*.gz 2>/dev/null);do
     sqlf=${f/gz/sql3}
     sf=${f/igprofMEM/MEMsql}
     logf=${sf/gz/log}
-    igprof-analyse --sqlite -v -d -g -r MEM_LIVE $f >$f.tmp 2> $logf 3>&2
-    ./fix-igprof-sql.py $f.tmp | sqlite3 $sqlf 2>> $logf 3>&2
+    igprof-analyse --sqlite -v -d -g -r MEM_LIVE $f >$f.tmp 2> $logf 3>&2 && ./fix-igprof-sql.py $f.tmp | sqlite3 $sqlf 2>> $logf 3>&2 &
 ## --For ascii-based report
     rf=${f/igprof/RES_}
     txtf=${rf/gz/txt.gz}
-    igprof-analyse  -v -d -g -r MEM_LIVE $f | gzip -c > $txtf 2>> $logf 3>&2
+    igprof-analyse  -v -d -g -r MEM_LIVE $f | gzip -c > $txtf 2>> $logf 3>&2 &
 done
+wait
--- Gen_tool/profile_igpp.sh
+++ Gen_tool/profile_igpp.sh
@@ -6,18 +6,16 @@ for f in $(ls *CPU*.gz 2>/dev/null);do
     sqlf=${f/gz/sql3}
     sf=${f/igprof/}
     logf=${sf/gz/log}
-    igprof-analyse --sqlite -v -d -g $f >$f.tmp 2> $logf 3>&2
-    ./fix-igprof-sql.py $f.tmp |  sqlite3 $sqlf 2>> $logf 3>&2
+    igprof-analyse --sqlite -v -d -g $f >$f.tmp 2> $logf 3>&2 && ./fix-igprof-sql.py $f.tmp |  sqlite3 $sqlf 2>> $logf 3>&2 &
 ## --For ascii-based report
     rf=${f/igprof/RES_}
     txtf=${rf/gz/txt.gz}
-    igprof-analyse  -v -d -g $f | gzip -c > $txtf 2>> $logf 3>&2
+    igprof-analyse  -v -d -g $f | gzip -c > $txtf 2>> $logf 3>&2 &
 done
 
 if [ -f RES_CPU_step3.txt.gz ]; then
-  gzip -dc RES_CPU_step3.txt.gz >RES_CPU_step3.txt
   export IGREP=RES_CPU_step3.txt
   export IGSORT=sorted_RES_CPU_step3.txt
-  awk -v module=doEvent 'BEGIN { total = 0; } { if(substr($0,0,1)=="-"){good = 0;}; if(good&&length($0)>0){print $0; total += $3;}; if(substr($0,0,1)=="["&&index($0,module)!=0) {good = 1;} } END { print "Total: "total } ' ${IGREP} | sort -n -r -k1 | awk '{ if(index($0,"Total: ")!=0){total=$0;} else{print $0;} } END { print total; }' > ${IGSORT} 2>&1
-  rm RES_CPU_step3.txt
+  gzip -dc RES_CPU_step3.txt.gz >RES_CPU_step3.txt && awk -v module=doEvent 'BEGIN { total = 0; } { if(substr($0,0,1)=="-"){good = 0;}; if(good&&length($0)>0){print $0; total += $3;}; if(substr($0,0,1)=="["&&index($0,module)!=0) {good = 1;} } END { print "Total: "total } ' ${IGREP} | sort -n -r -k1 | awk '{ if(index($0,"Total: ")!=0){total=$0;} else{print $0;} } END { print total; }' > ${IGSORT} 2>&1 && rm RES_CPU_step3.txt &
 fi
+wait
