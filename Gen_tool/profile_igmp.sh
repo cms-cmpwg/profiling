82a896b81ec52b253ec4a7196f79e8b6a1a81a26
Patrick Gartung <gartung@fnal.gov>
2025-03-10 16:17:36 +0100

    Remove fix-igprof-sql.py which is erroring out

--- Gen_tool/profile_igmp.sh
+++ Gen_tool/profile_igmp.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
-wget https://raw.githubusercontent.com/cms-sw/cms-bot/master/fix-igprof-sql.py
-chmod +x fix-igprof-sql.py
 for f in $(ls *MEM*.gz 2>/dev/null);do
 ## --For web-based report
     sqlf=${f/gz/sql3}
     sf=${f/igprofMEM/MEMsql}
     logf=${sf/gz/log}
-    igprof-analyse --sqlite -v -d -g -r MEM_LIVE $f >$f.tmp 2> $logf 3>&2 && ./fix-igprof-sql.py $f.tmp | sqlite3 $sqlf 2>> $logf 3>&2 &
+    igprof-analyse --sqlite -v -d -g -r MEM_LIVE $f | sqlite3 $sqlf 2>> $logf 3>&2 &
 ## --For ascii-based report
     rf=${f/igprof/RES_}
     txtf=${rf/gz/txt.gz}
--- Gen_tool/profile_igpp.sh
+++ Gen_tool/profile_igpp.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/cms-sw/cms-bot/master/fix-igprof-sql.py
-chmod +x fix-igprof-sql.py
 for f in $(ls *CPU*.gz 2>/dev/null);do
 ## --For web-based report
     sqlf=${f/gz/sql3}
     sf=${f/igprof/}
     logf=${sf/gz/log}
-    igprof-analyse --sqlite -v -d -g $f >$f.tmp 2> $logf 3>&2 && ./fix-igprof-sql.py $f.tmp |  sqlite3 $sqlf 2>> $logf 3>&2 &
+    igprof-analyse --sqlite -v -d -g $f |  sqlite3 $sqlf 2>> $logf 3>&2 &
 ## --For ascii-based report
     rf=${f/igprof/RES_}
     txtf=${rf/gz/txt.gz}
