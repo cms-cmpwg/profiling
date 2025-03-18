f127cd90572cac738b5825e7d96e0e372d259d67
Patrick Gartung <gartung@fnal.gov>
2025-03-18 21:01:31 +0100

    Set newer default workflow

--- Gen_tool/runall_vtune.sh
+++ Gen_tool/runall_vtune.sh
@@ -10,7 +10,7 @@ if [ "X$ARCHITECTURE" != "X" ]; then
 fi
 
 if [ "X$PROFILING_WORKFLOW" == "X" ];then
-  export PROFILING_WORKFLOW="23834.21"
+  export PROFILING_WORKFLOW="29834.21"
 fi
 
 if [ "X$WORKSPACE" != "X" ]; then
